ed675b77d33c19cf9c8fc5e56d0e98d963024d8a
Paselan91 <kg03272016@gmail.com>
2022-09-29 16:49:11 +0900

    inputクラスへvalidatorを実装

--- src/app/GraphQL/User/Query/FetchUserByIdQuery/FetchUserByIdInput.php
+++ src/app/GraphQL/User/Query/FetchUserByIdQuery/FetchUserByIdInput.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace App\GraphQL\User\Query\FetchUserByIdQuery;
 
+use App\Domain\User\Entity\UserEntity;
+use Exception;
+use Illuminate\Support\Facades\Validator;
+
 final class FetchUserByIdInput
 {
     /**
@@ -20,6 +24,8 @@ final class FetchUserByIdInput
      */
     public static function build(array $args): self
     {
+        self::validate($args);
+
         return new self(
             (int) $args['id']
         );
@@ -31,5 +37,38 @@ final class FetchUserByIdInput
     public function getId(): int
     {
         return $this->id;
+    }
+
+    /**
+     * @return void
+     */
+    private static function validate(array $args): void
+    {
+        Validator::make($args,
+            self::rules(),
+            self::attributes(),
+        )->validate();
+    }
+
+    /**
+     * @return array<string, array<mixed>>
+     */
+    private static function rules(): array
+    {
+        return [
+            'id' => ['exists:users,id'],
+        ];
+    }
+
+    /**
+     * @return array<string, string>
+     */
+    private static function attributes(): array
+    {
+        $userNmae = UserEntity::NAME;
+
+        return [
+            'id' => "{$userNmae}ID"
+        ];
     }
 }
